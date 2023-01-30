1f31dd81089c30a2b119efbbe9d07584589c32dc
GretaFormica <greta.formica@gmail.com>
2023-01-30 16:31:00 +0100

    snack 2

---
+++ snack 2/script.js
@@ -0,0 +1,13 @@
+//array vuoto
+const array = [];
+let numero;
+
+let sum = 0;
+
+while (sum < 50) {
+    prompt("inserisci un numero");
+    numero = prompt("inserisci un numero");
+    array.push(mumero);
+    sum = sum + numero
+    console.log(array)
+}
