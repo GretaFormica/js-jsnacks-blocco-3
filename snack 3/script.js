82e34c6302c0eea03a239dc48918a9e7357c51a8
GretaFormica <greta.formica@gmail.com>
2023-01-30 16:40:18 +0100

    snack 3

---
+++ snack 3/script.js
@@ -0,0 +1,22 @@
+//dichiarazioni costanti
+const N = prompt("inserisci un numero");
+
+let i = 0;
+
+while (i == N) {
+
+    let y = 0
+
+    while (y == 10) {
+        const array = [];
+        const numero = Math.floor(Math.random() * 100) + 1;
+        array.push(numero);
+
+        y++;
+    }
+
+    console.log[array];
+    i++
+
+
+}
